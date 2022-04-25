9d83d606ad2660daa60c9fa9a2ffd578215a45b8
by275 <90392275+by275@users.noreply.github.com>
2022-04-25 23:32:11 +0900

    separate alpine builds for each platform (#30)
    * separate alpine builds for each platform
    * force release stage dependent on test stage
    
    When using buildx and multi-stage build, if the last stage is independent to its previous stage, then error in the previous stage is ignored and the whole build process finishes without error.
    
    * shorten LIBTORRENT_VER to LT_VER

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LIBTORRENT_VER="$1"
+LT_VER="$1"
 TAGS="$2"
 U_ID=$(/usr/bin/id -u)
 G_ID=$(/usr/bin/id -g)
@@ -8,11 +8,11 @@ G_ID=$(/usr/bin/id -g)
 mkdir -p $(pwd)/release
 
 for TAG in $TAGS; do
-    BASE_IMAGE="ghcr.io/by275/libtorrent:${LIBTORRENT_VER}-${TAG}"
+    BASE_IMAGE="ghcr.io/by275/libtorrent:${LT_VER}-${TAG}"
     for manifest in $(docker buildx imagetools inspect --raw ${BASE_IMAGE} | jq -r '.manifests[] | @base64'); do
         arch=$(echo $manifest | base64 --decode | jq -r '.platform.architecture')
         digest=$(echo $manifest | base64 --decode | jq -r '.digest')
-        file="/release/libtorrent-${LIBTORRENT_VER}-${TAG}-$arch.tar.gz"
+        file="/release/libtorrent-${LT_VER}-${TAG}-$arch.tar.gz"
         if [[ $TAG == "alpine"* ]]; then
             docker run --rm -v $(pwd)/release:/release ${BASE_IMAGE}@${digest} \
                 sh -c "apk add tar && tar -C /libtorrent-build -zcvf $file usr && chown $U_ID:$G_ID $file"
