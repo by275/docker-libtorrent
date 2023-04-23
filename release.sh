8f069b2d35abbb52dd86d5c9f302ba2bb96265da
by275 <90392275+by275@users.noreply.github.com>
2023-04-23 14:34:33 +0900

    add support for ubuntu 23.04 (#52)

--- release.sh
+++ release.sh
@@ -5,19 +5,19 @@ TAGS="$2"
 U_ID=$(/usr/bin/id -u)
 G_ID=$(/usr/bin/id -g)
 
-mkdir -p $(pwd)/release
+mkdir -p "$(pwd)/release"
 
 for TAG in $TAGS; do
     BASE_IMAGE="ghcr.io/by275/libtorrent:${LT_VER}-${TAG}"
-    for manifest in $(docker buildx imagetools inspect --raw ${BASE_IMAGE} | jq -r '.manifests[] | @base64'); do
-        arch=$(echo $manifest | base64 --decode | jq -r '.platform.architecture')
-        digest=$(echo $manifest | base64 --decode | jq -r '.digest')
+    for manifest in $(docker buildx imagetools inspect --raw "${BASE_IMAGE}" | jq -r '.manifests[] | @base64'); do
+        arch=$(echo "$manifest" | base64 --decode | jq -r '.platform.architecture')
+        digest=$(echo "$manifest" | base64 --decode | jq -r '.digest')
         file="/release/libtorrent-${LT_VER}-${TAG}-$arch.tar.gz"
         docker build -t lt:release -<<EOF
 FROM alpine
 RUN apk add --no-cache tar
 COPY --from=${BASE_IMAGE}@${digest} /libtorrent-build /lt
 EOF
-        docker run --rm -v $(pwd)/release:/release lt:release sh -c "tar -C /lt -zcvf $file usr && chown $U_ID:$G_ID $file"
+        docker run --rm -v "$(pwd)/release:/release" lt:release sh -c "tar -C /lt -zcvf $file usr && chown $U_ID:$G_ID $file"
     done
 done
